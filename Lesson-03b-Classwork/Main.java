47bfcbcc94eeeccfa7618e0a777bde155dc3e9c6
johncrisostomob <johncrisostomob@nycstudents.net>
2025-09-11 13:28:22 +0000

    .

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("John Crisostomo Barkas");
+    System.out.println("John Crisostomo Barkas \n 203-18 26th avenue");
+    System.out.print("John Crisostomo Barkas , 203-18 26th avenue");
+    
+  }
+
+  
+}
