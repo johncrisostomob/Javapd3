874ed7cce9ac784d71c5926c51facb61b74a7842
johncrisostomob <johncrisostomob@nycstudents.net>
2025-10-01 13:32:52 +0000

    ,,

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -16,6 +16,18 @@ class Main {
 	print("Hello");
   }
   double F to C( double F ){
-	double c = (F-32)*9/5.0;
+	double C = (F-32)*9/5.0;
+  }
+  double sphereVolume( double r ){
+  double v = 4/3.0*Math.PI*r*r*r;
+  return v;
+  }
+  double coneVolume(double r, double h){
+    double V = 1.0/3*Math.PI*r*r*h;
+    return v;
+  }
+  double distance(double x1, double y1, double x2, double y2){
+    double d = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+    return d;
   }
 }
