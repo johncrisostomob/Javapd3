b5fdb36a455c3f5e0b329df6d6c54d66c36c740d
johncrisostomob <johncrisostomob@nycstudents.net>
2025-10-01 13:16:40 +0000

    ...

---
+++ Lesson-08-Activity/Main.java
@@ -0,0 +1,21 @@
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+
+  }
+  
+  void print(String msg){
+	System.out.println(msg);
+
+  }
+  void init(){
+	print("Hello");
+  }
+  double F to C( double F ){
+	double c = (F-32)*9/5.0;
+  }
+}
