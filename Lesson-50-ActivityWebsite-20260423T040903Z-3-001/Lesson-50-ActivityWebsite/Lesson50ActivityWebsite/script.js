68237b1cec6d0600f26181c1c61af9f7dbf768cf
johncrisostomob <johncrisostomob@nycstudents.net>
2026-04-23 04:47:54 +0000

    a

--- Lesson-50-ActivityWebsite-20260423T040903Z-3-001/Lesson-50-ActivityWebsite/Lesson50ActivityWebsite/script.js
+++ Lesson-50-ActivityWebsite-20260423T040903Z-3-001/Lesson-50-ActivityWebsite/Lesson50ActivityWebsite/script.js
@@ -3,7 +3,33 @@ let data,songs;
 function init(){
   $.ajaxSetup({async: false});
   
- 
+  let link= "http://localhost:8500"
+  let route= "/songs";
+  let songs = $.getJSON(link+route).responseJSON;
+  
+  generateCards(songs);
+}
+
+
+  // b) Write a function that accepts an array of JSON that contains song information, generates an info card for each track(song) and displays them on the webpage. Invoke(call) this function within the init() after getting the data. (See picture "WebPagePicture.PNG" to determine the types of HTML elements to use).
+function generateCards(songs){
+  let output = document.getElementById("output");
+  let build ="";
+
+  for(let i=0; i<songs.length; i++){
+    let song = songs[i];
+    build += `<div class="card" >`;
+    build +=   `<h2>Song Name</h2>`;
+    build +=   `<p>${song.Name}</p>`;
+    build +=   `<h3> Album </h3>`;
+    build +=   `<p>${song.Title}</p>`;
+    build +=   `<h3> Composer </h3>`;
+	  build +=   `<p>${song.Composer}</p>`;
+	  build +=   `<hr>`;
+    build += `</div>`;
+  }
+
+  output.innerHTML = build;
 }
 
 
